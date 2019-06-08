eaf7a1c9a6acb473eb568182bbc021e9d84bd8e2
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2019-06-08 12:41:13 +0200

    Added lesson 01 end state

--- src/presentation.js
+++ src/presentation.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Deck } from 'spectacle';
+import { Deck, Slide, Heading, Text } from 'spectacle';
 
 export default class Presentation extends React.Component {
     render() {
-        return <Deck />;
+        return (
+            <Deck>
+                <Slide>
+                    <Heading>Learn spectacle</Heading>
+                    <Text>by @tlakomy</Text>
+                </Slide>
+                <Slide>
+                    <Text>
+                        {`To add a slide to your presentation, use a <Slide /> component`}
+                    </Text>
+                </Slide>
+                <Slide>
+                    <Text>{`To add text, use <Heading /> or <Text /> components`}</Text>
+                </Slide>
+            </Deck>
+        );
     }
 }
