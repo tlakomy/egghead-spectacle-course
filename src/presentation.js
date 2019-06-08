0c819e765382947426eeb88049df744b66994d44
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2019-06-08 13:17:49 +0200

    Added lesson 02 end state

--- src/presentation.js
+++ src/presentation.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { Deck, Slide, Heading, Text } from 'spectacle';
 
+import createTheme from 'spectacle/lib/themes/default';
+
+const theme = createTheme(
+    {
+        primary: '#141518',
+        secondary: '#FAFBFC',
+        heading: 'papayawhip',
+        text: '#E98F97'
+    },
+    {
+        bestFont: 'Comic Sans MS'
+    }
+);
+
 export default class Presentation extends React.Component {
     render() {
         return (
-            <Deck>
+            <Deck theme={theme}>
                 <Slide>
                     <Heading>Learn spectacle</Heading>
-                    <Text>by @tlakomy</Text>
+                    <Text textColor="text">by @tlakomy</Text>
                 </Slide>
-                <Slide>
-                    <Text>
+
+                <Slide bgColor="secondary">
+                    <Text textColor="text">
                         {`To add a slide to your presentation, use a <Slide /> component`}
                     </Text>
                 </Slide>
+
                 <Slide>
-                    <Text>{`To add text, use <Heading /> or <Text /> components`}</Text>
+                    <Text textColor="text">{`To add text, use <Heading /> or <Text /> components`}</Text>
+                </Slide>
+
+                <Slide>
+                    <Heading textColor="heading">Themes</Heading>
+                    <Text textFont="bestFont" textColor="text">
+                        To create a theme use createTheme(colors, fonts)
+                        function
+                    </Text>
                 </Slide>
             </Deck>
         );
