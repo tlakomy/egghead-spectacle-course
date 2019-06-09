3108617e1df766bcea61e023f0ed331860f5a3ed
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2019-06-09 17:39:32 +0200

    Added lesson 11 start state

---
+++ src/examples/counterExample.js
@@ -0,0 +1,20 @@
+const counterExample = `import React, { useState } from 'react';
+
+import './Counter.css';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <section className="counter">
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+        </section>
+    );
+};
+
+export default Counter;
+`;
+
+export default counterExample;
